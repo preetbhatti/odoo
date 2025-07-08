e5d57ee9523f9d75bb2ba9d1fd3e80e33ebcd9fb
preetbhatti <prt.c.bhatti@gmail.com>
2025-07-08 17:18:40 +0530

    [ADD][form_edit_button_restore] Add module.

---
+++ form_edit_button_restore/static/src/js/form_edit_button.js
@@ -0,0 +1,27 @@
+/** @odoo-module **/
+import { patch } from "@web/core/utils/patch";
+import { FormController } from "@web/views/form/form_controller";
+
+patch(FormController.prototype, {
+    setup() {
+        super.setup();
+        this.model.config.mode = "readonly";
+    },
+    edit(params) {
+        var self = this;
+        if(this.canEdit && this.model.config.mode === "readonly") {
+            this.model.root.config.mode = "edit";
+            this.model.load().then(() => {
+                self.render(true);
+            })
+        }
+    },
+    async saveAndClose() {
+        var res = await this.save();
+        if (res) { this.model.config.mode = "readonly" }
+    },
+    async discardAndClose() {
+        await this.discard();
+        this.model.config.mode = "readonly";
+    }
+});
